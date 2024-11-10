1acbf26a924a8f9d521e9b2713087c09d8321afe
Артем <user@WIN-QERL5BT18CI>
2024-11-10 16:16:12 +0500

    create Ad component + minimize ad card

--- web/src/pages/client/AdSearchPageClient.js
+++ web/src/pages/client/AdSearchPageClient.js
@@ -11,12 +11,13 @@ import SortingDialogClient from '../../components/SortingDialogClient';
 import Card from '../../components/Card';
 import {useSearchService} from '../../data/SearchService';
 import { useState } from 'react';
-import { useAdService } from '../../data/AdService';
+import { useAdService } from '../../data/AdServiceArtem';
 import Avatar from '../../components/Avatar';
 import StarsBar from '../../components/StarsBar';
 import Header from '../../components/Header';
 import FilterDialogClient from '../../components/FilterDialogClient';
 import Switch from '../../components/Switch';
+import Ad from '../../components/Ad';
 
 export default function AdSearchPageClient() {
     const appContext=useAppContext();
@@ -63,27 +64,13 @@ export default function AdSearchPageClient() {
                 <ScrollingList onBottomReached={()=>console.log('wow')}>
                     {adsInScrollingList.map((ad, index) => 
                         <Card id={ad.id} key={ad.id}>
-                            <div className="ad">
-                                <div className="ad__img-presenter" style={{gridRow:'span 2'}}>
-                                    <img src={ad.img} style={{width:'250px',height:'250px',borderRadius:'10%'}}/>
-                                </div>
-                                <div className="ad__text-info">
-                                    <Header level={1}>{ad.title}</Header>
-                                    <Subcaption level={2}>{userService.getUserById(ad.userId).location}</Subcaption>
-                                    <Subcaption level={1} color={'text'}>{ad.description}</Subcaption>
-                                </div>
-                                <Switch checked={appContext.loginedUser.favoriteAds.includes(ad.id)}/>
-                                <div className="ad__user-info">
-                                    <Avatar src={userService.getUserById(ad.userId).avatar}/>
-                                    <div className="ad__user-info__c2">
-                                        <Header level={5}>{userService.getUserById(ad.userId).firstname} {userService.getUserById(ad.userId).lastname}</Header>
-                                        <div className="ad__user-info__c2__r2">
-                                            <StarsBar input_mode={false} value={userService.getUserById(ad.userId).rating}/>
-                                            <Subcaption level={2} color={'secondary'}>Отзывы</Subcaption>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            <Ad user={userService.getUserById(ad.userId)}
+                            img='https://alpinabook.ru/upload/resize_cache/iblock/8d9/550_800_1/8d9cd63476f15e85f0d8796555ab1e6b.jpg'
+                            title={ad.title}
+                            description={ad.description}
+                            checked={appContext.loginedUser.favoriteAds.includes(ad.id)}
+                            price={ad.price}
+                            />
                         </Card>
                     )}
                 </ScrollingList>
